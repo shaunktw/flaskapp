d105ceecccf1f0a12fdf65a37e847869d5dc6a3d
Shaun <shaunktw@gmail.com>
2014-02-03 20:33:31 -0500

    modified flask app to include resume page

--- app/routes.py
+++ app/routes.py
@@ -10,5 +10,9 @@ def home():
 def about():
 	return render_template('about.html')
 
+@app.route('/resume')
+def resume():
+	return render_template('resume.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
