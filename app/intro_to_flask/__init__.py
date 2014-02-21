ad4c793e418f1164d2714454190c28113a49d949
Shaun <shaunktw@gmail.com>
2014-02-21 14:06:06 -0500

    added db scripts for create and migrate

--- app/intro_to_flask/__init__.py
+++ app/intro_to_flask/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'chang3m3!'
+app.config.from_object('conifg')
+db = SQLAlchemy(app)
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
@@ -21,3 +24,4 @@ from routes import mail
 mail.init_app(app)
 
 import intro_to_flask.routes
+import intro_to_flask.models
